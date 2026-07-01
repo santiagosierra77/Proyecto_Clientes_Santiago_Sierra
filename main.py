c35769dfbaaaed87c0b26a6301abaf3034922c66
santiagosierra77 <santiagosierram17@gmail.com>
2026-07-01 15:33:47 -0500

    Crear facturas editado en parte, nuto con el modelo factura

--- main.py
+++ main.py
@@ -5,57 +5,58 @@ from modelos.transacciones import Transaccion, TransaccionCrear, TransaccionEdit
 
 app = FastAPI()
 
-
 lista_clientes: list[Cliente] = []
 lista_facturas: list[Factura] = []
 lista_transacciones: list[Transaccion] = []
 
 
-# endpoint, para obtener o listar todos los clientes
+# Endpoint, para obtener o listar todos los clientes
 @app.get("/clientes", response_model=list[Cliente])
 async def listar_clientes():
     return lista_clientes
 
 
-# endpoint, para obtener o listar un solo cliente de la lista
+# Endpoint, para obtener o listar un solo cliente de la lista
 @app.get("/clientes/{cliente_id}", response_model=Cliente)
 async def listar_cliente(cliente_id: int):
-    #recorrer la lista_clientes
+    # Recorrer la lista_clientes
     for i, obj_cliente in enumerate(lista_clientes):
         if obj_cliente.id == cliente_id:
             return obj_cliente
     raise HTTPException(
-        status_code=400, detail=f"El cliente con id {cliente_id}, no existe.   "
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"El cliente con id {cliente_id}, no existe."
     )
 
 
-# endpoint, para crear un cliente y agregar a la lista
+# Endpoint, para crear un cliente y agregar a la lista
 @app.post("/clientes", response_model=Cliente)
 async def crear_cliente(datos_cliente: ClienteCrear):
     cliente_val = Cliente.model_validate(datos_cliente.model_dump())
-    #generar id 
+    # Generar id 
     id_cliente = len(lista_clientes)+1
     cliente_val.id = id_cliente
     lista_clientes.append(cliente_val)
     return cliente_val
 
 
-# endpoint, para editar un cliente y agregar a la lista
+# Endpoint, para editar un cliente y agregar a la lista
 @app.patch("/clientes/{cliente_id}", response_model=Cliente)
 async def editar_cliente(cliente_id: int, datos_cliente: ClienteEditar):
     for i, obj_cliente in enumerate(lista_clientes):
         if obj_cliente.id == cliente_id:
-            #validar cliente
+            # Validar cliente
             cliente_val = Cliente.model_validate(datos_cliente.model_dump())
             cliente_val.id = cliente_id
             lista_clientes[i] = cliente_val
             return cliente_val
     raise HTTPException(
-        status_code=400,detail=f"El cliente con id {cliente_id}, no existe."
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"El cliente con id {cliente_id}, no existe."
     )
 
 
-# endpoint eliminar cliente
+# Endpoint eliminar cliente
 @app.delete("/clientes/{cliente_id}", response_model=Cliente)
 async def eliminar_cliente(cliente_id: int):
     for i, obj_cliente in enumerate(lista_clientes):
@@ -63,11 +64,12 @@ async def eliminar_cliente(cliente_id: int):
             cliente_eliminado = lista_clientes.pop(i)
             return cliente_eliminado
     raise HTTPException(
-        status_code=400,detail=f"El cliente con id {cliente_id}, no existe."
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"El cliente con id {cliente_id}, no existe."
     )
 
 # |||||||||||||||||||||||||||||
-# crear los endpoint para facturas
+# Crear los endpoint para facturas
 
 
 @app.get("/facturas", response_model=list[Factura])
@@ -77,18 +79,36 @@ async def listar_facturas():
 
 @app.get("/facturas/{factura_id}", response_model=Factura)
 async def listar_factura(factura_id: int):
-     #recorrer la lista_facturas
+     # Recorrer la lista_facturas
     for i, obj_factura in enumerate(lista_facturas):
         if obj_factura.id == factura_id:
             return obj_factura
     raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST, detail=f"La factura con id {factura_id}, no existe.   "
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"La factura con id {factura_id}, no existe."
     )
 
 
-@app.post("/facturas/{id_cliente}", response_model=Factura)
-async def crear_factura(id_cliente: int, datos_factura: Factura):
-    pass
+@app.post("/facturas/{cliente_id}", response_model=Factura)
+async def crear_factura(cliente_id: int, datos_factura: FacturaCrear):
+    # Buscar el cliente
+    cliente_encontrado = None
+    for cliente in lista_clientes:
+        if cliente.id == cliente_id:
+            cliente_encontrado = cliente
+    # Mensaje si no existe el cliente
+    if not cliente_encontrado:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail=f"El cliente con id {cliente_id}, no existe."
+            )
+
+    # Validar datos de la factura
+    factura_val = Factura.model_validate(datos_factura.model_dump())
+    factura_val.cliente = cliente_encontrado
+    # Id de la factura
+    factura_val.id = len(lista_facturas)+1
+    return factura_val
 
 
 @app.patch("/facturas/{id_factura}", response_model=Factura)
@@ -102,7 +122,7 @@ async def eliminar_factura(id_factura):
 
 
 #||||||||||||||||||||||||||
-# crear los endpoint para transacciones 
+# Crear los endpoint para transacciones 
 
 
 @app.get("/transacciones", response_model=list[Transaccion])
--- modelos/facturas.py
+++ modelos/facturas.py
@@ -1,5 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
+
+from .transacciones import Transaccion
 from .clientes import Cliente
+from datetime import datetime
 
 # El decorador @property proviene de Python y sirve para convertir un método de una clase en una propiedad de solo lectura.
 # Validación Pydantic v2, @computed_field es un decorador que te permite definir propiedades o métodos que se calculan dinámicamente a partir de otros campos y se incluyen automáticamente en la respuesta JSON de tu API.
@@ -8,10 +11,15 @@ from .clientes import Cliente
 
 # Crear el modelo transacciones(id, fecha, vr_total, cliente)
 class FacturaBase(BaseModel):
-    fecha: str 
-    vr_total: float #calcular(cantidad * vr_unitario)
+    fecha: str = datetime.now()
     cliente: Cliente #esta es la relacion con el cliente(objeto)
-
+    transacciones: list[Transaccion] = []
+    
+    @computed_field
+    @property
+    def vr_total(self) -> float:
+        #calcular(cantidad * vr_unitario)
+        return 222
 
 class FacturaCrear(FacturaBase):
     pass
